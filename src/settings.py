7bb9b847159a738ff03f5b5d98170d3b192bd916
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-05 15:46:25 +0330

    fix: bug fix in installed apps list.

--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'products.app.ProductsConfig',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
