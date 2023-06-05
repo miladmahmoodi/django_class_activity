c8b71e02375ab54042cc2354bbd986d75979bc35
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-05 14:22:05 +0330

    build: build admin classes for product modules.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ProductOption)
+class ProductOptionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.SellerProductPrice)
+class SellerProductPriceAdmin(admin.ModelAdmin):
+    pass
