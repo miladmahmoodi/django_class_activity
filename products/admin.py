142294d962670d3ffa7d4bee0609c0318425033f
Milad Mahmoodi <info@miladmahmoodi.ir>
2023-06-05 15:44:14 +0330

    feat: product and category updated.

--- products/admin.py
+++ products/admin.py
@@ -2,14 +2,62 @@ from django.contrib import admin
 from . import models
 
 
+class ImageInline(admin.TabularInline):
+    model = models.Image
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'pk',
+        'en_name',
+        'name',
+        'category',
+    ]
+    list_filter = [
+        'category',
+    ]
+    search_fields = [
+        'en_name',
+        'name',
+    ]
+
+    inlines = [
+        ImageInline,
+    ]
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'pk',
+        'name',
+        'slug',
+        'parent',
+    ]
+    list_filter = [
+        'parent',
+    ]
+    search_fields = [
+        'name',
+        'description',
+    ]
+    fieldsets = (
+        ('Details', {
+            'fields': (
+                'name',
+                'slug',
+                'description',
+                'parent',
+            ),
+        }),
+        ('Images', {
+            'fields': (
+                'icon',
+                'image',
+            ),
+        }),
+    )
 
 
 @admin.register(models.Image)
@@ -27,9 +75,15 @@ class AnswerAdmin(admin.ModelAdmin):
     pass
 
 
+class AnswerInline(admin.TabularInline):
+    model = models.Answer
+
+
 @admin.register(models.Question)
 class QuestionAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        AnswerInline,
+    ]
 
 
 @admin.register(models.ProductOption)
